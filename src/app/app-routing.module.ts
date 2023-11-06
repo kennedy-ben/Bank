5954299211eec8be1534a6481618e0e775042c48
kennedy-ben <makuanikennedy@gmail.com>
2023-11-06 17:17:48 +0300

    created bank app and added mutiple components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigupComponent } from './sigup/sigup.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sigup',component: SigupComponent},
+  {path: 'login',component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
